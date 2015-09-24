9260f1bbb2441123a64ee08f376db118b0bb9b85
Half-A-Tool <halfatool@hotmail.com>
2015-09-23 21:23:54 -0500

    Update TextController.cs

--- Mingo-the-Dingo/Assets/Scripts/TextController.cs
+++ Mingo-the-Dingo/Assets/Scripts/TextController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+int Users;
+
 	// Use this for initialization
 	void Start () {
-	
+	Users=3;
 	}
 	
 	// Update is called once per frame
